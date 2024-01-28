4275dc218300b44adafcdabca162121cec54a1d3
prashantkhadkamagar <121386896+prashantkhadkamagar@users.noreply.github.com>
2024-01-28 23:40:56 +0545

    update user profile

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -100,7 +100,27 @@ const getUserProfile = asyncHandler(async (req, res) => {
 
 //update own profile
 const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
 
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(401); 
+    throw new Error("USER NOT FOUND");
+  }
 });
 
 // get all the users -ADMIN ONLY
